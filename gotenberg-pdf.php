3b70fe92ea9139b75a58b0b3bfee36bae9732dc8
nasedk.in <nasedkin@me.com>
2019-08-30 02:51:31 +0300

    prototype with theming

--- gotenberg-pdf.php
+++ gotenberg-pdf.php
@@ -13,12 +13,16 @@
  * GitHub Plugin URI: https://github.com/nasedkinpv/gotenberg-pdf
  * 
  */
+
 require "vendor/autoload.php";
 include_once "options-page.php";
 
 use TheCodingMachine\Gotenberg\Client;
 use TheCodingMachine\Gotenberg\URLRequest;
 use TheCodingMachine\Gotenberg\Request;
+// use TheCodingMachine\Gotenberg\Request\setAssets;
+
+use TheCodingMachine\Gotenberg\DocumentFactory;
 
 if (!defined("ABSPATH")) {
     exit; // Exit if accessed directly
@@ -30,65 +34,71 @@ class Gotenberg
     const DEBUG = 1;
     private $webserver = 'http://nginx:80';
     private $gotenberg = 'http://gotenberg:3000';
-    private $query_print = '?print';
-    private $query_pdf = 'pdftest';
+    private $query_print = 'print';
+    private $query_pdf = 'pdf';
     private $post_type = 'yacht';
+    private $theme = 'sanlorenzo';
 
     function __construct()
     {
         $options = get_option('gotenberg_pdf_settings');
         $this->webserver = $options['webserver_uri'] ?: $this->webserver;
         $this->pdf_folder = get_stylesheet_directory() . '/print/';
+        $this->mode = isset($_REQUEST[$this->query_pdf]) ? $_REQUEST[$this->query_pdf] : null;
+        $this->print = isset($_REQUEST[$this->query_print]) ? $_REQUEST[$this->query_print] : null;
     }
-    function watch_query_and_post_type()
+    function watch_query_and_post_type($html)
     {
-        if (get_post_type() == $this->post_type and $_REQUEST[$this->query_pdf]) {
+        if ($this->print == 1) {
+            include_once(plugin_dir_path(__FILE__) . '/themes/' . $this->theme . '/index.php');
+            exit;
+        }
+        if (get_post_type() == $this->post_type and $this->mode) {
             $this->request_uri = self::get_uri_request();
             $this->file_exist = self::check_pdf_exist();
             $this->filename = self::get_pdf_filename();
             $this->file_path = $this->pdf_folder . $this->filename;
             $this->file_uri = get_stylesheet_directory_uri() . '/print/' . $this->filename;
-            if ($this::DEBUG) var_dump($this);
-            switch ($_REQUEST[$this->query_pdf]) {
-                case 1:
-                    // regular download, if not exist create, if exist download.
-                    echo 'regular download ';
-                    // check
-                    if (!$this->file_exist) {
-                        echo 'file not exist, create new ';
+            // if ($this::DEBUG) var_dump($this);
+            if ($this->mode) {
+                header('Content-type:application/pdf');
+                header('Content-Disposition:attachment; filename="' . $this->filename . '"');
+                switch ($this->mode) {
+                    case 1:
+                        // regular download, if not exist create, if exist download.
+                        if ($this->file_exist) {
+                            readfile($this->file_path);
+                        } else {
+                            $this->generateFromUri($this->request_uri, $this->file_path);
+                            readfile($this->file_path);
+                        }
+                        break;
+                    case 2:
+                        // overwrite
                         $this->generateFromUri($this->request_uri, $this->file_path);
-                        // $this->load_pdf($this->file_path);
-                    } else {
-                        // $this->load_pdf($this->file_path);
-                        // @TODO CHECK FOR TIME AND RECREATE? 
-                    }
-                    echo '<script type="text/javascript">setTimeout(function(){ window.location.href = `' . $this->file_uri . '`}, 2000);</script>';
-                    wp_die();
-                    break;
-                case 2:
-                    // overwrite: remove, create, download.
-                    echo 'overwrite/create new, download ';
-                    $this->generateFromUri($this->request_uri, $this->file_path);
-                    echo '<script type="text/javascript">setTimeout(function(){ window.location.href = `' . $this->file_uri . '`}, 2000);</script>';
-                    wp_die();
-                    break;
-                case 3:
-                    // custom brokerage generation, file name with postfix, always regenerate and download.
-                    break;
-                case 'debug':
-                    var_dump($this);
-                    wp_die();
-                    break;
-                default:
-                    # code...
-                    break;
+                        readfile($this->file_path);
+                        break;
+                    default:
+                        $this->generateFromUri($this->request_uri, $this->file_path);
+                        readfile($this->file_path);
+                        break;
+                }
+                exit;
             }
+        } else {
+            return $html;
         }
+    }
+    public function load()
+    {
+        add_filter('template_redirect', [$this, 'watch_query_and_post_type']);
+        // add_action('admin_menu', [$this, 'add_plugin_options_page']);
+        // add_action('admin_init', [$this, 'add_plugin_settings']);
     }
     function get_uri_request()
     {
         $request = $this->webserver . str_replace(get_home_url(), '', get_permalink());
-        $request .= $this->query_print;
+        $request .= '?' . $this->query_print . '=1';
         return $request;
     }
     function check_pdf_exist()
@@ -108,12 +118,7 @@ class Gotenberg
         $filename .= '-' . get_the_ID() . '.pdf';
         return $filename;
     }
-    public function load()
-    {
-        add_filter('the_content', [$this, 'watch_query_and_post_type']);
-        // add_action('admin_menu', [$this, 'add_plugin_options_page']);
-        // add_action('admin_init', [$this, 'add_plugin_settings']);
-    }
+
     function generateFromUri($url, $filepath)
     {
         $client = new Client($this->gotenberg, new \Http\Adapter\Guzzle6\Client());
@@ -123,9 +128,11 @@ class Gotenberg
         $request->setMargins(URLRequest::NO_MARGINS);
         $request->setWaitDelay(2.5);
         $request->setWaitTimeout(10);
+
         $client->store($request, $filepath);
         return $request;
     }
 }
+
 $plugin = new Gotenberg();
 $plugin->load();
